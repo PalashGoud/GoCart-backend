e54ddbbe8a845100f6241000e5af3c83b06366a4
Palash Goud <palashgoud607@gmail.com>
2025-04-10 14:36:13 +0530

    commit

--- models/Order.js
+++ models/Order.js
@@ -5,6 +5,8 @@ const OrderSchema = new mongoose.Schema({
     products: [{ productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },  quantity: Number}],
     status: { type: String, enum: ['pending', 'completed', 'cancelled'], default: 'pending' },
     totalPrice: Number,
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    address: {type: String,required: true},
+
 });
 module.exports = mongoose.model('Order', OrderSchema);
